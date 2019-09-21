231f7d88d53eb08a2da88ff656790d6dd2394c94
马勇 <1173962595@qq.com>
2019-09-21 12:47:05 +0800

    合并k个有序列表，分治算法

--- Code/23.合并k个排序链表.swift
+++ Code/23.合并k个排序链表.swift
@@ -14,9 +14,95 @@
  *     }
  * }
  */
+
+   public class ListNode {
+     public var val: Int
+     public var next: ListNode?
+     public init(_ val: Int) {
+         self.val = val
+         self.next = nil
+     }
+   }
+
 class Solution {
-    func mergeKLists(_ lists: [ListNode?]) -> ListNode? {
+    
+    /// 逐一比较法，此算法swift会超时
+    func mergeKListSolution1(_ lists: [ListNode?]) -> ListNode? {
         
+        let head = ListNode(0)
+        var cur: ListNode? = head
+        var lists = lists
+        while true {
+           var minIndex = -1
+           /// 找到当前链表的最小节点给cur
+           for index in 0 ..< lists.count {
+               guard lists[index] != nil else { continue }
+               if minIndex == -1 || lists[minIndex]!.val > lists[index]!.val {
+                   minIndex = index
+               }
+           }
+           /// 如果此时minIndex = -1 则表示已经遍历结束
+           if minIndex == -1 { break }
+           cur?.next = lists[minIndex]
+           cur = cur?.next
+           lists[minIndex] = lists[minIndex]?.next
+        }
+        return head.next
+    }
+    
+    func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        let node: ListNode = ListNode(0)
+        var cur: ListNode? = node
+        
+        var templ1 = l1
+        var templ2 = l2
+        while templ1 != nil, templ2 != nil {
+            if templ1!.val < templ2!.val {
+                cur?.next = templ1
+                cur = cur?.next
+                templ1 = templ1?.next
+            } else {
+                cur?.next = templ2
+                cur = cur?.next
+                templ2 = templ2?.next
+            }
+        }
+
+        if templ1 == nil {
+            cur?.next = templ2
+        } else if templ2 == nil {
+            cur?.next = templ1
+        }
+        return node.next
+    }
+    
+    /// 两两合并（每个链表都和第一个链表合并）
+    func mergeKListsSolution2(_ lists: [ListNode?]) -> ListNode? {
+        guard lists.count > 0 else { return lists.first as? ListNode }
+        var lists = lists
+        for index in 0 ..< lists.count - 1 {
+          lists[0] = mergeTwoLists(lists[0], lists[index + 1])
+        }
+        return lists[0]
+    }
+    
+    /// 分治
+    func mergeKLists(_ lists: [ListNode?]) -> ListNode? {
+        guard lists.count > 0 else { return lists.first as? ListNode }
+
+        var lists = lists
+        var step = 1
+        
+        while step < lists.count {
+            let nextStop = step << 1
+            var index = 0
+            while index + step < lists.count {
+                lists[index] = mergeTwoLists(lists[index], lists[index + step])
+                index += nextStop
+            }
+            step = nextStop
+        }
+        return lists[0]
     }
 }
 
