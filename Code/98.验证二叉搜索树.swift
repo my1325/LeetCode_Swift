656d6b597d9791f98358606fb10f2fdf6c90f136
马勇 <1173962595@qq.com>
2019-09-26 17:47:40 +0800

    验证二叉搜索树

--- Code/98.验证二叉搜索树.swift
+++ Code/98.验证二叉搜索树.swift
@@ -19,13 +19,29 @@
 class Solution {
     /// 中序遍历
     var lastVal: Int?
-    func isValidBST(_ root: TreeNode?) -> Bool {
+    func isValidBST1(_ root: TreeNode?) -> Bool {
         guard let node = root else { return true }
         
         if !isValidBST(node.left) { return false }
         if let lVal = lastVal, lVal >= node.val { return false }
         lastVal = node.val
         if !isValidBST(node.right) { return false }
+        return true
+    }
+    
+    /// 界定值大小
+    func isValidBST(_ root: TreeNode?) -> Bool {
+        return isValidBST(root, max: nil, min: nil)
+    }
+    
+    private func isValidBST(_ node: TreeNode?, max: Int?, min: Int?) -> Bool {
+        guard let node = node else { return true }
+        
+        if !isValidBST(node.left, max: node.val, min: min) { return false }
+        if !isValidBST(node.right, max: max, min: node.val) { return false }
+        
+        if let max = max, node.val >= max { return false }
+        if let min = min, node.val <= min { return false }
         return true
     }
 }
