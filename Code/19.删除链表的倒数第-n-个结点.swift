7730fafa6d9da4f77d13c3f0e0480d16ceedc0eb
my <>
2021-03-31 17:54:29 +0800

    删除倒数第n个结点

---
+++ Code/19.删除链表的倒数第-n-个结点.swift
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode.cn id=19 lang=swift
+ *
+ * [19] 删除链表的倒数第 N 个结点
+ *
+ * https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/description/
+ *
+ * algorithms
+ * Medium (41.46%)
+ * Likes:    1298
+ * Dislikes: 0
+ * Total Accepted:    355.7K
+ * Total Submissions: 857.9K
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+ * 
+ * 进阶：你能尝试使用一趟扫描实现吗？
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：head = [1,2,3,4,5], n = 2
+ * 输出：[1,2,3,5]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：head = [1], n = 1
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：head = [1,2], n = 1
+ * 输出：[1]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 链表中结点的数目为 sz
+ * 1 
+ * 0 
+ * 1 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
+        return _removeNthFromEnd(head, n)
+    }
+
+    var count: Int = 0
+    func _removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
+        guard head != nil else { return nil }
+        var last: ListNode? = nil
+        if let next = head?.next {
+            last = _removeNthFromEnd(next, n)
+            head?.next = last
+        }
+
+        count += 1
+
+        if count == n {
+            head?.next = nil
+            return last
+        }
+        return head
+    }
+}
+// @lc code=end
+
