838337958e00702a7b2cbe419f3f3fa5b98b2e4b
my <>
2021-04-02 10:16:37 +0800

    24

---
+++ Code/24.两两交换链表中的节点.swift
@@ -0,0 +1,92 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=swift
+ *
+ * [24] 两两交换链表中的节点
+ *
+ * https://leetcode-cn.com/problems/swap-nodes-in-pairs/description/
+ *
+ * algorithms
+ * Medium (69.41%)
+ * Likes:    869
+ * Dislikes: 0
+ * Total Accepted:    240.5K
+ * Total Submissions: 346.5K
+ * Testcase Example:  '[1,2,3,4]'
+ *
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ * 
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：head = [1,2,3,4]
+ * 输出：[2,1,4,3]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：head = []
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：head = [1]
+ * 输出：[1]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 链表中节点的数目在范围 [0, 100] 内
+ * 0 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：你能在不修改链表节点值的情况下解决这个问题吗?（也就是说，仅修改节点本身。）
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func swapPairs(_ head: ListNode?) -> ListNode? {
+        guard head != nil else { return head }
+
+        let newHead = ListNode(0)
+        newHead.next = head
+
+        var cur: ListNode? = newHead
+        var next = cur?.next 
+
+        while let node = next?.next {
+            next?.next = node.next 
+            cur?.next = node 
+            node.next = next 
+
+            cur = next 
+            next = next?.next
+        }
+
+        return newHead.next
+    }
+}
+// @lc code=end
+
