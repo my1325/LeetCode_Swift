bfb6df98853d03d370746d5432b6dc28d9ff8b71
my <>
2021-04-06 12:58:36 +0800

    110 平衡二叉树

---
+++ Code/110.平衡二叉树.swift
@@ -0,0 +1,97 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=swift
+ *
+ * [110] 平衡二叉树
+ *
+ * https://leetcode-cn.com/problems/balanced-binary-tree/description/
+ *
+ * algorithms
+ * Easy (55.47%)
+ * Likes:    652
+ * Dislikes: 0
+ * Total Accepted:    193.5K
+ * Total Submissions: 348.7K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ * 
+ * 本题中，一棵高度平衡二叉树定义为：
+ * 
+ * 
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：true
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = [1,2,2,3,3,null,null,4,4]
+ * 输出：false
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：true
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中的节点数在范围 [0, 5000] 内
+ * -10^4 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func isBalanced(_ root: TreeNode?) -> Bool {
+        return _heightForTree(root) != -1
+    }
+
+    func _heightForTree(_ root: TreeNode?) -> Int {
+        guard let _root = root else { return 0 }
+
+        let leftHeight = _heightForTree(_root.left)
+        if leftHeight == -1 {
+            return -1
+        }
+
+        let rightHeight = _heightForTree(_root.right)
+
+        if rightHeight == -1 {
+            return -1
+        }
+
+        return abs(leftHeight - rightHeight) <= 1 ? max(leftHeight, rightHeight) + 1 : -1
+    }
+}
+// @lc code=end
+
