d3ccf70213195e1b8dbdf164977b9908e67067f0
my <>
2021-04-15 18:17:23 +0800

    209

---
+++ Code/209.长度最小的子数组.swift
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode.cn id=209 lang=swift
+ *
+ * [209] 长度最小的子数组
+ *
+ * https://leetcode-cn.com/problems/minimum-size-subarray-sum/description/
+ *
+ * algorithms
+ * Medium (45.43%)
+ * Likes:    611
+ * Dislikes: 0
+ * Total Accepted:    132.4K
+ * Total Submissions: 290.5K
+ * Testcase Example:  '7\n[2,3,1,2,4,3]'
+ *
+ * 给定一个含有 n 个正整数的数组和一个正整数 target 。
+ * 
+ * 找出该数组中满足其和 ≥ target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr]
+ * ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：target = 7, nums = [2,3,1,2,4,3]
+ * 输出：2
+ * 解释：子数组 [4,3] 是该条件下的长度最小的子数组。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：target = 4, nums = [1,4,4]
+ * 输出：1
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：target = 11, nums = [1,1,1,1,1,1,1,1]
+ * 输出：0
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * 1 
+ * 1 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：
+ * 
+ * 
+ * 如果你已经实现 O(n) 时间复杂度的解法, 请尝试设计一个 O(n log(n)) 时间复杂度的解法。
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+
+    func minSubArrayLen(_ target: Int, _ nums: [Int]) -> Int {
+        var _min = 0
+        var start: Int = 0
+        var end: Int = 0
+        var sum: Int = 0
+        while end < nums.count {
+            sum += nums[end]
+            while sum >= target {
+                if _min == 0 {
+                    _min = end - start + 1
+                } else {
+                    _min = min(_min, end - start + 1)
+                }
+                sum -= nums[start]
+                start += 1
+            }
+            end += 1
+        }
+        return _min
+    }
+}
+// @lc code=end
+
