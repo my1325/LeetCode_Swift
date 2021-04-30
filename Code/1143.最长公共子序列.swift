e4ea59f910c3122a5f7210a0cabc8fcf362138d0
my <>
2021-04-30 13:18:09 +0800

    1143

---
+++ Code/1143.最长公共子序列.swift
@@ -0,0 +1,92 @@
+/*
+ * @lc app=leetcode.cn id=1143 lang=swift
+ *
+ * [1143] 最长公共子序列
+ *
+ * https://leetcode-cn.com/problems/longest-common-subsequence/description/
+ *
+ * algorithms
+ * Medium (61.40%)
+ * Likes:    534
+ * Dislikes: 0
+ * Total Accepted:    114.7K
+ * Total Submissions: 184K
+ * Testcase Example:  '"abcde"\n"ace"'
+ *
+ * 给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
+ *
+ * 一个字符串的 子序列
+ * 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+ *
+ *
+ * 例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
+ *
+ *
+ * 两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：text1 = "abcde", text2 = "ace"
+ * 输出：3
+ * 解释：最长公共子序列是 "ace" ，它的长度为 3 。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：text1 = "abc", text2 = "abc"
+ * 输出：3
+ * 解释：最长公共子序列是 "abc" ，它的长度为 3 。
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：text1 = "abc", text2 = "def"
+ * 输出：0
+ * 解释：两个字符串没有公共子序列，返回 0 。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1
+ * text1 和 text2 仅由小写英文字符组成。
+ *
+ *
+ */
+
+// @lc code=start
+class Solution {
+    func longestCommonSubsequence(_ text1: String, _ text2: String) -> Int {
+        var _t1: [Character] = []
+        for char in text1 {
+            _t1.append(char)
+        }
+
+        var _t2: [Character] = []
+        for char in text2 {
+            _t2.append(char)
+        }
+
+        var dp: [[Int]] = Array(repeating: Array(repeating: 0, count: _t2.count + 1), count: _t1.count + 1)
+
+        for i in 1 ... _t1.count {
+            for j in 1 ... _t2.count {
+                if _t1[i - 1] == _t2[j - 1] {
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                } else {
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+                }
+            }
+        }
+        return dp[_t1.count][_t2.count]
+    }
+}
+
+// @lc code=end
